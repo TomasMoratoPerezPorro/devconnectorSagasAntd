004eda78d0a48e3bde4d6bae4a7a549b1b350e59
Tomas Morato <tomas.morato@between.tech>
2021-04-12 15:25:08 +0200

    Initial folder structure and Ant-design setup

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from 'antd';
+import React from 'react';
+
+import './styles.less';
+
+function App() {
+  return (
+    <div className='App'>
+      <Button type='primary'>Button</Button>
+    </div>
+  );
+}
+
+export default App;
