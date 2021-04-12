c1646a82c49ad60be28895f1df78c5a358845203
Tomas Morato <tomas.morato@between.tech>
2021-04-12 15:39:56 +0200

    Added Eslint config

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,21 @@
-const CracoAntDesignPlugin = require('craco-antd');
+const CracoLessPlugin = require('craco-less')
+const { ESLINT_MODES } = require('@craco/craco')
 
 module.exports = {
-  plugins: [{ plugin: CracoAntDesignPlugin }],
-};
+  eslint: {
+    mode: ESLINT_MODES.file
+  },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#d43742' },
+            javascriptEnabled: true
+          }
+        }
+      }
+    }
+  ]
+}
