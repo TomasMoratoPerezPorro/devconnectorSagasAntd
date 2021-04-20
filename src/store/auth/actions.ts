8154f505ace684ab70129d00a6908eb159d93a98
Tomas Morato <tomas.morato@between.tech>
2021-04-20 11:22:06 +0200

    Register fail saga and alert message

--- src/store/auth/actions.ts
+++ src/store/auth/actions.ts
@@ -2,6 +2,6 @@ import { createAsyncAction } from '../../helpers/common'
 
 import { IUserInfo, IUserRegisterInfo } from '../../models/common'
 
-export const registerUser = createAsyncAction<IUserRegisterInfo, IUserInfo | null, undefined>(
+export const registerUser = createAsyncAction<IUserRegisterInfo, IUserInfo | null, any>(
   'AUTH Register User'
 )
--- src/store/auth/sagas.ts
+++ src/store/auth/sagas.ts
@@ -1,16 +1,40 @@
 import { all, call, put, takeLatest } from '@redux-saga/core/effects'
-import { v4 as uuidv4 } from 'uuid'
 import services from '../../services'
-import { IAlertObject } from '../alert/actions'
 import rootActions from '../rootActions'
+import { v4 as uuidv4 } from 'uuid'
+import { IAlertObject } from '../alert/actions'
 
 function* requestRegister(action: ReturnType<typeof rootActions.authActions.registerUser.request>) {
   try {
     const { data } = yield call(services.userAPI.registerUser, action.payload)
     yield put(rootActions.authActions.registerUser.success(data))
   } catch (err) {
-    /* console.log('ERROR' + JSON.stringify(err.response.data.errors)) */
-    /* const errors = err.response.data.errors
+    console.error('HELLO' + JSON.stringify(err))
+    yield put(rootActions.authActions.registerUser.failure(err.response.data.errors))
+  }
+}
+
+function* failedRegister(action: ReturnType<typeof rootActions.authActions.registerUser.failure>) {
+  const errors = action.payload
+  let id = uuidv4()
+  let alert: IAlertObject = {
+    msg: errors[0].msg,
+    alertType: 'danger',
+    timeOut: 5000,
+    id: id
+  }
+  yield put(rootActions.alertActions.setAlert(alert))
+}
+
+export default function* allSagas() {
+  yield all([
+    takeLatest(rootActions.authActions.registerUser.request, requestRegister),
+    takeLatest(rootActions.authActions.registerUser.failure, failedRegister)
+  ])
+}
+
+/* console.log('ERROR' + JSON.stringify(err.response.data.errors)) */
+/* const errors = err.response.data.errors
     if (errors) {
       errors.forEach((error: any) => {
         let id = uuidv4()
@@ -23,10 +47,3 @@ function* requestRegister(action: ReturnType<typeof rootActions.authActions.regi
         yield put(rootActions.alertActions.setAlert(alert))
       })
     } */
-    yield put(rootActions.authActions.registerUser.failure(err.response.data.errors))
-  }
-}
-
-export default function* allSagas() {
-  yield all([takeLatest(rootActions.authActions.registerUser.request, requestRegister)])
-}
